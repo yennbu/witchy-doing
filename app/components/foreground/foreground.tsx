9e5a6ec9c45d9fc2953331ee21da8920ca5a5718
Yenn <yb.burgman@gmail.com>
2025-11-10 10:42:59 +0100

    changed cat img to gif

--- app/components/foreground/foreground.tsx
+++ app/components/foreground/foreground.tsx
@@ -5,17 +5,17 @@ export default function Foreground() {
     return (
         <div className="foreground">
             <Image
-                src="/cat.png"
-                alt="Illustration of a cat"
-                className="img cat"
-                width={200}
-                height={200}
-            />
-            <Image
                 src="/mushrooms.png"
                 alt="Illustration of mushrooms, pupkins and ferns"
                 className="img mushrooms"
-                width={250}
+                width={300}
+                height={200}
+            />
+            <Image
+                src="/cat.gif"
+                alt="Animation of a cat blinking"
+                className="img cat"
+                width={230}
                 height={200}
             />
         </div>
